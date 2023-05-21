3b7d645447c231f1d630b20d0ad1dece36c7ab71
조상호 <hyundai_sangho@naver.com>
2023-05-21 23:07:23 +0900

    개인정보 이용 약관 모달 추가

--- index.php
+++ index.php
@@ -16,6 +16,32 @@ if (isset($_SESSION['unique_id'])) {
 
 ?>
 
+<!-- jquery 모달 css -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
+
+<style>
+  /* ( 크롬, 사파리, 오페라, 엣지 ) 동작 */
+  .scroll::-webkit-scrollbar {
+    display: none;
+  }
+
+  .scroll {
+    -ms-overflow-style: none;
+    /* 인터넷 익스플로러 */
+    scrollbar-width: none;
+    /* 파이어폭스 */
+  }
+
+
+  /* 스크롤바 생성 */
+  .scroll {
+    width: 400px;
+    height: 400px;
+    overflow-y: scroll;
+  }
+</style>
+
 <body>
 
   <div class="wrapper">
@@ -49,13 +75,26 @@ if (isset($_SESSION['unique_id'])) {
 
         <div class="field input">
           <label>비밀번호</label>
-          <input type="password" name="password" id="password" required />
-          <i class="fas fa-eye" class="eyePassword"></i>
+          <input type="password" id="signupPassword" class="password" required />
+          <i class="fas fa-eye"></i>
         </div>
 
-        <div class="field image">
-          <label>사진 선택</label>
-          <input type="file" name="image" id="signupImageFile" accept="image/*" />
+        <div style="display: flex; justify-content: space-between;">
+          <div class="field image">
+            <label>사진 선택</label>
+            <input type="file" name="image" id="signupImageFile" accept="image/*" />
+          </div>
+          <div>
+            <img id="signupImage" style="display:none; width: 50px; height: 50px; object-fit: cover; position: relative; top: 5px;">
+          </div>
+        </div>
+
+        <div style="margin-top: 15px; margin-bottom: 5px;" id="signupPrivacy">
+          <input type="checkbox" id="signupPrivacyCheckbox" />
+
+          <a href="#signupPrivacyModal" rel="modal:open">
+            <span style="position: relative; top: -0.7px; cursor: pointer;">개인정보 이용약관 동의(필수)</span>
+          </a>
         </div>
 
         <div class="field button">
@@ -66,6 +105,38 @@ if (isset($_SESSION['unique_id'])) {
       <div class=" link">이미 가입했다면? <a href="login.php">로그인</a>
       </div>
     </section>
+
+    <!-- 햄버거 버튼 모달-->
+    <div id="signupPrivacyModal" class="modal">
+      <div class="scroll">
+        <p style="width: 400px; height: 400px; word-break: keep-all;">
+
+          1. 개인 정보의 수집 항목 및 수집 방법<br><br>
+          채팅 사이트에서는 기본적인 회원 서비스 제공을 위한 필수 정보로
+          다음의 정보를 수집하고 있습니다. 필수 정보를 입력해주셔야 회원 서비스 이용이 가능합니다.<br><br>
+
+          가. 수집하는 개인 정보의 항목 <br><br>
+          * 수집하는 필수 항목<br>
+          - 가입 정보 : 성명, 이메일<br><br>
+
+          나. 개인 정보 수집 방법<br><br>
+          홈페이지 회원 가입을 통한 수집 <br><br>
+
+          2. 개인 정보의 수집/이용 목적 및 보유/이용 기간<br><br>
+          채팅 사이트에서는 정보 주체의 회원 가입일로부터 서비스를 제공하는 기간 동안에 한하여
+          채팅 서비스를 이용하기 위한 최소한의 개인정보를 보유 및 이용 하게 됩니다.<br><br>
+
+          회원가입 등을 통해 개인정보의 수집·이용, 제공 등에 대해 동의하신 내용은 언제든지 철회하실 수 있습니다.
+
+        </p>
+      </div>
+    </div>
+
+    <!-- jquery.js 추가 :) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+
+    <!-- jquery modal js 추가 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 
     <!-- Sweet Alert 팝업 -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
