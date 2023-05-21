40da61d5a32b55f6d618ecd31563cdff8731cde7
조상호 <hyundai_sangho@naver.com>
2023-05-21 23:09:46 +0900

    authocode 테이블 추가 및 messages 테이블 구조 변경

--- sql/chatapp.sql
+++ sql/chatapp.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- 생성 시간: 23-05-05 10:36
--- 서버 버전: 10.4.22-MariaDB
--- PHP 버전: 8.1.2
+-- 생성 시간: 23-05-19 17:44
+-- 서버 버전: 10.4.28-MariaDB
+-- PHP 버전: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 테이블 구조 `authcode`
+--
+
+CREATE TABLE `authcode` (
+  `id` int(11) NOT NULL,
+  `randomString` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 테이블의 덤프 데이터 `authcode`
+--
+
+INSERT INTO `authcode` (`id`, `randomString`) VALUES
+(1, '3827215539'),
+(2, '1412509232'),
+(3, '8088886265'),
+(4, '8756068561'),
+(5, '7587845448'),
+(6, '2082828756'),
+(7, '8364226641'),
+(8, '6942544386'),
+(9, '3432992206'),
+(10, '9472401591');
+
+-- --------------------------------------------------------
+
+--
 -- 테이블 구조 `messages`
 --
 
@@ -32,8 +59,10 @@ CREATE TABLE `messages` (
   `incoming_msg_id` int(255) NOT NULL,
   `outgoing_msg_id` int(255) NOT NULL,
   `msg` varchar(1000) NOT NULL,
-  `msg_time` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `msg_time` datetime NOT NULL DEFAULT current_timestamp(),
+  `msg_read` varchar(20) NOT NULL DEFAULT 'no',
+  `msg_image` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -47,13 +76,19 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `img` varchar(255) NOT NULL,
+  `img` mediumblob NOT NULL,
   `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- 덤프된 테이블의 인덱스
 --
+
+--
+-- 테이블의 인덱스 `authcode`
+--
+ALTER TABLE `authcode`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 테이블의 인덱스 `messages`
@@ -72,16 +107,22 @@ ALTER TABLE `users`
 --
 
 --
+-- 테이블의 AUTO_INCREMENT `authcode`
+--
+ALTER TABLE `authcode`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- 테이블의 AUTO_INCREMENT `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=341;
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=754;
 
 --
 -- 테이블의 AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
