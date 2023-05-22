74315d8413f000d8e5122e2ef32007efc8ca0404
조상호 <hyundai_sangho@naver.com>
2023-05-22 12:07:24 +0900

    fetch url을 php/verify-password.php에서 user/verify-password.php 변경

--- assets/js/verify-password.js
+++ assets/js/verify-password.js
@@ -3,10 +3,10 @@ const form = document.querySelector('form');
 form.addEventListener('submit', async (event) => {
 	event.preventDefault();
 
-	await fetch('php/verify-password.php')
+	await fetch('user/verify-password.php')
 		.then((response) => response.text())
 		.then((data) => {
-			if (data == '이름, 이메일, 인증 코드 쿠키 삭제 성공') {
+			if (data == '이름, 이메일 쿠키 삭제, 인증 코드 세션 삭제 성공') {
 				location.href = 'login.php';
 			}
 		});
