eed0ea8e41cb3f513d07824f557615514dae8f3b
조상호 <hyundai_sangho@naver.com>
2023-05-22 12:19:58 +0900

    채팅 화면에서 채팅 데이터 삭제시 처리되는 php 로직

---
+++ user/delete-message.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+// DELETE 메소드로 리퀘스트 요청이 들어왔을 때만 실행
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+
+  $deleteMessage = json_decode(file_get_contents("php://input"), true);
+
+  // chat.js에서 POST로 받아온 deleteId
+  $deleteId = $deleteMessage["deleteMessageId"];
+
+  include_once '../db/Database.php';
+
+  // 디비 객체 생성
+  $database = new Database();
+
+  $database->deleteMessage($deleteId);
+
+  // 디비 커넥션 끊기
+  $database->connectionClose();
+
+}
+
+// DELETE 메소드로 리퀘스트 요청을 보낸게 아니라 url을 직접 주소 표시줄에 입력해서 들어왔다면 홈 화면으로 이동시킴
+else {
+  header("Location: ../index.php");
+}
+
+
+?>
