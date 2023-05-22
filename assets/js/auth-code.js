5ac251584dc0199a1e3392f8bb62e3284eb6d792
조상호 <hyundai_sangho@naver.com>
2023-05-22 11:56:44 +0900

    form submit 이벤트 내부 fetch 시에 보내는 데이터를 formData에서 json 데이터로 변경

--- assets/js/auth-code.js
+++ assets/js/auth-code.js
@@ -1,12 +1,26 @@
+// 이메일 인증 코드 화면의 폼
 const form = document.querySelector('form');
+
+// 이메일 인증 코드 입력 후 에러 발생시 나오는 div 텍스트
 const errorText = document.querySelector('.error-txt');
 
+// 이메일 인증 코드 input
+const emailAuthCode = document.querySelector('#emailAuthCode');
+
+// 이메일 인증 코드 submit 이벤트 발생시
 form.addEventListener('submit', async (event) => {
+	// 폼이 넘어가는 것을 방지
 	event.preventDefault();
 
-	await fetch('php/auth-code.php', {
+	// 비동기로 user/auth-code.php 호출
+	await fetch('user/auth-code.php', {
 		method: 'POST',
-		body: new FormData(form),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			emailAuthCode: emailAuthCode.value,
+		}),
 	})
 		.then((response) => response.text())
 		.then((data) => {
@@ -15,7 +29,7 @@ form.addEventListener('submit', async (event) => {
 
 				errorText.innerHTML = data;
 				errorText.style.display = 'block';
-			} else {
+			} else if (data == '비밀번호 찾기 인증 성공') {
 				location.href = 'verify-password.php';
 			}
 		});
