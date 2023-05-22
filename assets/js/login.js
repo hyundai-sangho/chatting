f74704a67cc0237c2b33beae0995c1180aaebd53
조상호 <hyundai_sangho@naver.com>
2023-05-22 12:05:22 +0900

    fetch 시에 보내는 데이터를 FormData에서 json으로 변경

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,16 +9,23 @@ const loginMaintain = document.querySelector('#loginMaintain');
 // 로그인 상태 유지 텍스트
 const loginStatusMaintain = document.querySelector('#loginStatusMaintain');
 
-const loginChatIcon = document.querySelector('#loginChatIcon');
+const loginEmail = document.querySelector('#loginEmail');
+const loginPassword = document.querySelector('#loginPassword');
 
 // 로그인 버튼 클릭 또는 로그인창에서 엔터키를 눌렀을시 submit 이벤트 발생
 form.addEventListener('submit', async (event) => {
 	// 이벤트가 발생하는 것을 방지
 	event.preventDefault();
 
-	await fetch('php/login.php', {
+	await fetch('user/login.php', {
 		method: 'POST',
-		body: new FormData(form),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			loginEmail: loginEmail.value,
+			loginPassword: loginPassword.value,
+		}),
 	})
 		.then((response) => response.text())
 		.then((data) => {
@@ -28,33 +35,70 @@ form.addEventListener('submit', async (event) => {
 			}
 			// "성공"이 아니라면 에러 메시지를 받아와서 화면에 보여줌
 			else {
-				// 문제 발생시 로그인 화면의 채팅 아이콘 화면에서 가리기
-				loginChatIcon.style.display = 'none';
-
 				errorText.innerHTML = data;
 				errorText.style.display = 'block';
 			}
 		});
 });
 
-// 로그인 상태 유지 아이콘 클릭시 php/maintain-login.php로 email 데이터를 POST로 보내 이메일을 쿠키로 저장
+// 로그인 상태 유지 아이콘 클릭시 user/maintain-login.php로 email 데이터를 POST로 보내 이메일을 쿠키로 저장
 loginMaintain.addEventListener('click', async () => {
 	loginMaintain.classList.toggle('active');
 
-	// php/loginMaintain.php 파일로 POST로 폼 데이터로 form 값들을 집어넣어 보내버림
-	await fetch('php/maintain-login.php', {
-		method: 'POST',
-		body: new FormData(form),
-	});
+	if (loginEmail.value == '') {
+		alert('이메일을 입력해 주세요.');
+	} else if (loginPassword.value == '') {
+		alert('비밀번호를 입력해 주세요.');
+	} else {
+		// user/loginMaintain.php 파일로 이메일과 비밀번호를 json 형식으로 보냄
+		await fetch('user/maintain-login.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				loginEmail: loginEmail.value,
+				loginPassword: loginPassword.value,
+			}),
+		})
+			.then((response) => response.text())
+			.then((data) => {
+				if (data == '입력하신 이메일과 비밀번호를 다시 확인해 주세요.') {
+					alert(data);
+				} else {
+					console.log(data);
+				}
+			});
+	}
 });
 
-// 로그인 상태 유지 텍스트를 클릭시 php/maintain-login.php로 email 데이터를 POST로 보내 이메일을 쿠키로 저장
+// 로그인 상태 유지 텍스트를 클릭시 user/maintain-login.php로 email 데이터를 POST로 보내 이메일을 쿠키로 저장
 loginStatusMaintain.addEventListener('click', async () => {
 	loginMaintain.classList.toggle('active');
 
-	// php/loginMaintain.php 파일로 POST로 폼 데이터로 form 값들을 집어넣어 보내버림
-	await fetch('php/maintain-login.php', {
-		method: 'POST',
-		body: new FormData(form),
-	});
+	if (loginEmail.value == '') {
+		alert('이메일을 입력해 주세요.');
+	} else if (loginPassword.value == '') {
+		alert('비밀번호를 입력해 주세요.');
+	} else {
+		// user/loginMaintain.php 파일로 이메일과 비밀번호를 json 형식으로 보냄
+		await fetch('user/maintain-login.php', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				loginEmail: loginEmail.value,
+				loginPassword: loginPassword.value,
+			}),
+		})
+			.then((response) => response.text())
+			.then((data) => {
+				if (data == '입력하신 이메일과 비밀번호를 다시 확인해 주세요.') {
+					alert(data);
+				} else {
+					console.log(data);
+				}
+			});
+	}
 });
